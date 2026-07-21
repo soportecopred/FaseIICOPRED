287ffbcfb11f59c83c53777e72cbf09ea2dc4aff
soportecopred <soporte.copred@gmail.com>
2026-07-21 15:27:56 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,13 +6,12 @@ import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gsta
 
 // 🔥 REEMPLAZA ESTOS DATOS CON LOS DE TU PROYECTO FIREBASE 🔥
 const firebaseConfig = {
-  apiKey: "AIzaSyBlVNCFiorRmKgUuzPbbiLHAatsBiDl2G0",
-  authDomain: "fase-ii-copred.firebaseapp.com",
-  projectId: "fase-ii-copred",
-  storageBucket: "fase-ii-copred.firebasestorage.app",
-  messagingSenderId: "941466034940",
-  appId: "1:941466034940:web:9c0ba8e97e4022b03bed79",
-  measurementId: "G-328HRS2HXK"
+    apiKey: "AIzaSyBlVNCFiorRmKgUuzPbbiLHAatsBiDl2G0",
+    authDomain: "fase-ii-copred.firebaseapp.com",
+    projectId: "fase-ii-copred",
+    storageBucket: "fase-ii-copred.firebasestorage.app",
+    messagingSenderId: "941466034940",
+    appId: "1:941466034940:web:9c0ba8e97e4022b03bed79"
 };
 
 // Inicializar
